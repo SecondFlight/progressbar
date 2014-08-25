01800bbbe1e9e79b417fb4114483bae265dff15e
SecondFlight <qbgeekjtw@gmail.com>
2014-08-24 22:11:17 -0400

    finished plugin shell

--- src/io/github/secondflight/ProgressBar/MainPlugin.java
+++ src/io/github/secondflight/ProgressBar/MainPlugin.java
@@ -1,9 +1,12 @@
 package io.github.secondflight.ProgressBar;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -33,7 +36,26 @@ public class MainPlugin extends JavaPlugin implements Listener {
 	// Commands
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
 	{
-		
+		if (sender instanceof Player) {
+			Player player = (Player) sender;
+			if (commandLabel.equalsIgnoreCase("progressbar") || commandLabel.equalsIgnoreCase("pb")) {
+				
+				if (args.length == 0) {
+					// Command usage message
+					
+					player.sendMessage("-------------------------------------------");
+					player.sendMessage("Usage:");
+					player.sendMessage("");
+					player.sendMessage("/pb new");
+					player.sendMessage("    -- creates a new progress bar");
+					player.sendMessage("-------------------------------------------");
+					
+				}
+			}
+		} else {
+			// this is displayed if the command is called from the console
+			logger.warning("You must be a player to use this command.");
+		}
 		
 		
 		return false;
